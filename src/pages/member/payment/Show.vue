4434c05da04c10e2805364dba163b3bd7eeed9aa
ardanwungkul <ardanwungkul14@gmail.com>
2025-10-21 09:40:34 +0700

    payment

--- src/pages/member/payment/Show.vue
+++ src/pages/member/payment/Show.vue
@@ -19,7 +19,20 @@ onMounted(async () => {
       <div class="rounded-lg bg-white shadow px-5 pb-3 pt-5 w-full">
         <p class="font-semibold text-lg">Detail Pembayaran</p>
         <p class="text-sm text-gray-500">Kode Referensi : {{ route.params.id }}</p>
-        <div class="flex justify-between items-center"></div>
+        <div v-for="(item, index) in payment?.details" :key="index">
+          <div class="flex justify-between items-center">
+            <div>
+              <p>
+                {{ item?.bill?.member?.name }}
+              </p>
+              <p>
+                {{
+                  item?.bill?.bill_type == 'registration' ? 'Biaya Pendaftaran' : 'Iuran Bulanan'
+                }}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </MemberLayouts>
