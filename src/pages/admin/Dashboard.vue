d3be63de8b2a50000f7f9f981456800e95b52d6e
ardanwungkul <ardanwungkul14@gmail.com>
2025-11-04 16:29:30 +0700

    update

--- src/pages/admin/Dashboard.vue
+++ src/pages/admin/Dashboard.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AdminLayouts from '@/layouts/AdminLayouts.vue'
+import { useAuthStore } from '@/stores/auth'
 import { useDashboardStore } from '@/stores/dashboard'
 import { onMounted, ref } from 'vue'
 
 const dashboardStore = useDashboardStore()
+const authStore = useAuthStore()
 const datas = ref()
 
 function fetchData() {
@@ -45,7 +47,7 @@ onMounted(async () => {
               <i class="fa-solid fa-money-check-dollar-pen text-2xl text-white"></i>
             </div>
             <div class="flex flex-col items-center w-full font-montserrat">
-              <p class="text-3xl font-bold text-white">0</p>
+              <p class="text-3xl font-bold text-white">{{ datas?.payment ?? 0 }}</p>
               <p class="text-sm text-white">Pembayaran Tertunda</p>
             </div>
           </div>
@@ -60,19 +62,30 @@ onMounted(async () => {
               <i class="fa-solid fa-money-bills text-2xl text-white"></i>
             </div>
             <div class="flex flex-col items-center w-full font-montserrat">
-              <p class="text-3xl font-bold text-white">0</p>
+              <p class="text-3xl font-bold text-white">{{ datas?.bill ?? 0 }}</p>
               <p class="text-sm text-white">Tagihan</p>
             </div>
           </div>
         </div>
       </div>
-      <div class="grid grid-cols-2 gap-3">
+      <!-- <div class="grid grid-cols-2 gap-3">
         <div class="h-80 w-full rounded-lg shadow bg-white"></div>
         <div class="h-80 w-full rounded-lg shadow bg-white"></div>
       </div>
       <div class="grid grid-cols-3 gap-3">
         <div class="h-80 w-full rounded-lg shadow bg-white col-span-2"></div>
         <div class="h-80 w-full rounded-lg shadow bg-white"></div>
+      </div> -->
+      <div class="w-full rounded-lg shadow bg-white p-5">
+        <i class="fa-regular fa-house-night text-2xl text-rhino-950"></i>
+        <p class="text-3xl font-montserrat font-medium">
+          Selamat Datang,
+          <span class="font-semibold">{{ authStore?.decryptedUserData?.name }}</span> !!
+        </p>
+        <p class="text-xs text-gray-500">
+          Selamat datang di aplikasi resmi Victoria Basketball! Nikmati kemudahan dalam mengelola
+          jadwal latihan, mengikuti event, dan terus berkembang bersama komunitas basket terbaik.
+        </p>
       </div>
     </div>
   </AdminLayouts>
